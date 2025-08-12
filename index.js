cac921e7d8decab533ff74f6f40d5fa7cba52ebf
Avkaran Klair <avkaranklair@gmail.com>
2025-08-12 09:42:42 +0100

    Add .env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { app } = require("./app");
+require('dotenv').config()
 
-const port = 3000;
+const { app } = require("./app");
+const port = process.env.PORT;
+
 app.listen(port, () => {
   console.log(`FruityAPI is running on port ${port}`);
 });
